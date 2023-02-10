86e7799962c3bf59abd29767868ca94c7b834990
Michael Schori <michael@tie-international.com>
2023-02-10 14:18:29 +0100

    DONE: - Added link from todo to user

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
@@ -8,6 +10,7 @@ class Todo(models.Model):
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
