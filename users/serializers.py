3ea8dfcdb768eed2b9676f9e5231fb1025753ec1
Michael Schori <michael@tie-international.com>
2023-03-03 10:57:46 +0100

    DONE: - Added signup and login rest-points

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import User
 
@@ -10,4 +13,49 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        exclude = ['password', 'user_permissions', 'email_confirmed', 'groups']
+        fields = ('id', 'email', 'firstname', 'lastname', 'dated_joined', 'last_login')
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        allow_blank=False,
+        validators=[validate_password]
+    )
+
+    class Meta:
+        model = User
+        fields = ('password', 'email', 'firstname', 'lastname')
+        extra_kwargs = {
+            'firstname': {'required': True},
+            'lastname': {'required': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            firstname=validated_data['firstname'],
+            lastname=validated_data['lastname'],
+            is_active=True
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+
+class SigninSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
--- users/views.py
+++ users/views.py
@@ -1,14 +1,47 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, views
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, SignupSerializer, SigninSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class SignupView(generics.CreateAPIView):
     """
-    ViewSet for User.
+    View for Signup.
     """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    serializer_class = SignupSerializer
+
+    def post(self, request, *args, **kwargs):
+        create_state = self.create(request, *args, **kwargs)
+        if create_state.status_code != 201:
+            return create_state
+        user = User.objects.get(email=request.data['email'])
+        tokens = RefreshToken.for_user(user)
+        return Response(
+            {
+                'access_token': str(tokens.access_token),
+                'refresh_token': str(tokens),
+                'user': UserSerializer(user).data
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+
+class SigninView(views.APIView):
+    serializer_class = SigninSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        tokens = RefreshToken.for_user(user)
+        return Response(
+            {
+                'access_token': str(tokens.access_token),
+                'refresh_token': str(tokens),
+                'user': UserSerializer(user).data
+            },
+            status=status.HTTP_200_OK
+        )
